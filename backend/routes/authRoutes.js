26fb45621e90b149bc147157a9582ca020c1129c
Kavi-Dew-23 <dewapuraka@gmail.com>
2025-11-09 13:28:10 +0530

    resolve conflicts

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,5 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/authController.js";
-<<<<<<< HEAD
-=======
-import { registerUser } from "../controllers/authController.js";
->>>>>>> 49f2d8965dd11e0740bee7cbcd5767c9038990a3
 
 const router = express.Router();
 
