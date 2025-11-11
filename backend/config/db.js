02cd18eba64c5f072fa6ef4eee508f01d64a9442
tharaka10 <truwanmaduwantha@gmail.com>
2025-11-11 21:46:50 +0530

    update reservation

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '',
-  database: process.env.MYSQL_DATABASE || 'bookfair',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// const pool = mysql.createPool({
+//   host: process.env.MYSQL_HOST || 'localhost',
+//   user: process.env.MYSQL_USER || 'root',
+//   password: process.env.MYSQL_PASSWORD || '',
+//   database: process.env.MYSQL_DATABASE || 'bookfair',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
-export { pool };
+// export { pool };
