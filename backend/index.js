db3c47bd2a13769cc0f26a09f45b1b659d826692
tharaka10 <truwanmaduwantha@gmail.com>
2025-11-14 19:58:49 +0530

    hosting update

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,9 @@ const app = express();
 // ✅ Use CORS properly
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], // your React app URL
+    origin: ["http://localhost:5173", "http://localhost:5174", 
+      "https://publisher-portal.web.app",
+      "https://organizer-portal-bookfair.web.app"], // your React app URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
